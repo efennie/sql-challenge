843cd6a7fb96f50c4d5df7901fbd5a3bf0631d28
efennie <efennie@email.arizona.edu>
2021-05-15 16:46:37 -0700

    query 3

--- query.sql
+++ query.sql
@@ -13,3 +13,10 @@ WHERE hire_date >= '1986-01-01' AND hire_date < '1987-01-01'
 
 --3.List the manager of each department with the following information: 
 --department number, department name, the manager's employee number, last name, first name.
+
+SELECT d.dept_no, n.dept_name, d.emp_no, e.last_name, e.first_name
+FROM dept_managers d
+LEFT JOIN employee_info e
+ON d.emp_no = e.emp_no
+LEFT JOIN department_name n
+ON n.dept_no = d.dept_no;
